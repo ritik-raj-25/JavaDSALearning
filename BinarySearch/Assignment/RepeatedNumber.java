c7dd283f3ba16fe4cd64673c119438c0d51cb6c4
ritik-raj-25 <ritikbunty2511@gmail.com>
2024-05-18 15:25:46 +0530

    Some correction has been made

--- BinarySearch/Assignment/RepeatedNumber.java
+++ BinarySearch/Assignment/RepeatedNumber.java
@@ -6,7 +6,7 @@ public class RepeatedNumber {
         int lo=0, hi=n-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(arr[mid]==mid){
+            if(arr[mid]<=mid){
                 if(arr[mid-1]==arr[mid]) return arr[mid];
                 else hi=mid-1;
             }
@@ -15,7 +15,7 @@ public class RepeatedNumber {
         return -1;
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,6};
+        int[] arr = {1,2,2,2,3,4,5};
         System.out.println(repeatedNumber(arr));
     }
 }
